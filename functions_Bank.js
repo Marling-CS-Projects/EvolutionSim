ac98a0d70ceb1c6be57fc2930d9f1f427416f7a9
Nayt <nate.bradshaw5959@gmail.com>
2022-07-05 12:52:05 +0100

    3 functions that will be building blocks
    myRect(...)
    myCircle(...)
    myConstraint(...)
    all i need to make a creature to evolve :)

---
+++ functions_Bank.js
@@ -0,0 +1,59 @@
+function MyRect(x, y, w, h, options){ //we need to add options to this
+    this.body = Bodies.rectangle(x, y, w, h, options);
+
+    this.w = w;
+    this.h = h;
+
+    World.add(engine.world, [this.body]); //
+
+    this.show = function(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0, 0, this.w, this.h);
+        pop();
+    }
+}
+
+function MyCircle(x, y, r, options){
+    this.body = Bodies.circle(x, y, r, options);
+
+    this.r = r;
+
+    World.add(engine.world, [this.body]);
+
+    this.show = function(){
+        var pos = this.body.position;
+        //var angle = this.body.angle;
+        
+        push();
+        translate(pos.x, pos.y);
+        //rotate(angle);
+        rectMode(CENTER);
+        circle(0, 0, r*2);
+        pop();
+    }
+}
+
+function MyConsraint(body1, body2, length, stiffness){
+
+    var constr = Constraint.create({
+        bodyA: body1.body,
+        bodyB: body2.body,
+        length: length,
+        stiffness: stiffness
+    })
+    
+    World.add(engine.world, constr);
+
+    this.show = function(){
+        line(body1.body.position.x, 
+             body1.body.position.y, 
+             body2.body.position.x, 
+             body2.body.position.y);
+    }
+}
--- matter_and_p5_test.js
+++ matter_and_p5_test.js
@@ -6,13 +6,18 @@ var Engine = Matter.Engine, //this is to cut down on typing
     //Render = Matter.Render, //using p5's renderer and runner
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+    Constraint = Matter.Constraint,
+    Composite = Matter.Composite; 
 
 // create an engine
 var engine;
 var world;
 var boxes = [];
+
 var ground;
+
+var connectedBoxes = [];
+var boxConst;
 
 function setup() {
   createCanvas(400, 400);
@@ -20,6 +25,23 @@ function setup() {
   world = engine.world;
   Matter.Runner.run(engine);
   ground = new MyRect(200, 390, 400, 20, { isStatic: true });
+  var tempBox1 = new MyRect(50, 10, 10, 10, null);
+  var tempBox2 = new MyRect(50, 30, 10, 10, null);
+
+  connectedBoxes.push(tempBox1);
+  connectedBoxes.push(tempBox2);
+
+  boxConst = new MyConsraint(tempBox1, tempBox2, 50, 0.4);
+/*
+  var constr = Constraint.create({
+    bodyA: tempBox1.body,
+    bodyB: tempBox2.body,
+    length: 50,
+    stiffness: 0.4
+  })
+  World.add(engine.world, constr);
+  */
+
   //boxA = new MyRect(100, 100, 20, 50);
 }
 /*
@@ -29,15 +51,27 @@ function draw() {
 }
 */
 function mouseDragged(){
-  boxes.push(new MyRect(mouseX, mouseY, 20, 20, null)) //create a new box on click, pass null for no custom options :)
+  boxes.push(new MyCircle(mouseX, mouseY, 10, null)) //create a new box on click, pass null for no custom options :)
 }
 
 function draw(){
   background(51);
 
   ground.show();
+  boxConst.show();
 
   for (let i = 0; i< boxes.length; i++){
     boxes[i].show() //for each element in list render it
   }
+
+  for (let i = 0; i< connectedBoxes.length; i++){
+    connectedBoxes[i].show() //for each element in list render it
+  }
+
+  /*
+  line(connectedBoxes[0].body.position.x, 
+      connectedBoxes[0].body.position.y, 
+      connectedBoxes[1].body.position.x, 
+      connectedBoxes[1].body.position.y);
+      */
 }
